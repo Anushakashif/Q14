5173fa87cce9279d8ceac86158be1935ebd6b7d0
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-21 15:19:00 -0700

    Add files via upload

---
+++ Q14.js
@@ -0,0 +1,4 @@
+"use strict";
+// 14. Guest List: If you could invite anyone, living or decease, to dinner, who would you invite? Make a list that includes at least three people you'd like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner. 
+let guestArr = ["Vivian", "Dante", "Isabella", "Kai", "Lau"];
+guestArr.map((item) => (console.log(`Dear ${item}, you are invited to a dinner`)));
